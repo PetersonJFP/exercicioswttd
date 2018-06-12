437d5e2b2e2077e0d88f23cc6b9bb21ae409ad1d
Peterson Passos <peterson.jfp@gmail.com>
2018-06-11 21:10:52 -0300

    initial commit

---
+++ google-python-exercises/basic/teste.py
@@ -0,0 +1,35 @@
+import re
+# E. not_bad
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+def not_bad(s):
+    if 'not' in s and 'bad' in s and s.index('not') < s.index('bad'):
+        return re.sub('not.*bad', 'good', s)
+    else:
+        return s
+
+
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+def main():
+    print('not_bad')
+    test(not_bad('This movie is not so bad'), 'This movie is good')
+    test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+    test(not_bad('This tea is not hot'), 'This tea is not hot')
+    test(not_bad("It's bad yet not"), "It's bad yet not")
+
+
+if __name__ == '__main__':
+    main()
